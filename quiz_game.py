6c20dd9e42bfed2c30fa3a0b22d50cda8577fe02
pydiscern <128986501+pydiscern@users.noreply.github.com>
2023-03-30 17:56:44 +0200

    Update quiz_game.py
    In this implementation:
    
    We define a Question class to store the questions, choices, and correct answers.
    We create a question_pool list to store instances of the Question class.
    We define several functions: display_question to display the question and choices, get_user_input to get user input, evaluate_response to check if the user's answer is correct, and play_game to handle the game loop.
    We implement a simple scoring system that increments the player's score for each correct answer.
    We use a while loop to create a game loop that continues until the user decides to stop playing.
    You can further refine and expand this implementation by adding a timer, lifelines, and adjusting the difficulty level based on the player's progress.
    
    This updated question_pool contains a variety of questions covering Python fundamentals such as variables, data types, operators, loops, functions, and data structures. You can add or modify the questions as needed to cover any additional topics you'd like to include in the quiz.

--- quiz_game.py
+++ quiz_game.py
@@ -1,108 +1,57 @@
 import random
+import time
 
-# questions and answers
-questions = [
-    {
-        "question": "What is the correct way to define a variable in Python?",
-        "options": ["int x = 10;", "x := 10", "x = 10", "let x = 10"],
-        "answer": "x = 10"
-    },
-    {
-        "question": "Which of the following is a valid Python data type?",
-        "options": ["integer", "float", "double", "decimal"],
-        "answer": "float"
-    },
-    {
-        "question": "How do you create a list in Python?",
-        "options": ["my_list = (1, 2, 3)", "my_list = [1, 2, 3]", "my_list = {1, 2, 3}", "my_list = <1, 2, 3>"],
-        "answer": "my_list = [1, 2, 3]"
-    },
-    {
-        "question": "Which loop continues executing a block of code as long as a specified condition is true?",
-        "options": ["do-while loop", "for loop", "while loop", "until loop"],
-        "answer": "while loop"
-    },
-    {
-        "question": "What is the correct way to define a function in Python?",
-        "options": ["function my_function():", "def my_function():", "func my_function():", "define my_function():"],
-        "answer": "def my_function():"
-    },
-    {
-        "question": "Which of the following is a valid string in Python?",
-        "options": ['"Hello, World!"', "'Hello, World!'", "Hello, World!", "string('Hello, World!')"],
-        "answer": "'Hello, World!'"
-    },
-    {
-        "question": "Which of the following is NOT a valid way to create a dictionary in Python?",
-        "options": ["my_dict = dict()", "my_dict = {}", "my_dict = {key: value}", "my_dict = [key: value]"],
-        "answer": "my_dict = [key: value]"
-    },
-    {
-        "question": "How do you access the first element of a list named 'my_list'?",
-        "options": ["my_list[0]", "my_list(0)", "my_list(1)", "my_list[1]"],
-        "answer": "my_list[0]"
-    },
-]
-
-def ask_question(question):
-    print(question["question"])
-    for i, option in enumerate(question["options"]):
-        print(f"{i+1}. {option}")
-    answer = int(input("Select the correct option (1-4): ")) - 1
-    return question["options"][answer] == question["answer"]
-
-def main():
-    score = 0
-    random.shuffle(questions)
-
-    for question in questions:
-        if ask_question(question):
-            print("Correct!")
-            score += 1
-        else:
-            print("Incorrect. Better luck next time!")
-        print()
-
-    print(f"Your final score: {score}/{len(questions)}")
-    class Question:
-    def __init__(self, question, options, answer):
+class Question:
+    def __init__(self, question, choices, correct_answer):
         self.question = question
-        self.options = options
-        self.answer = answer
+        self.choices = choices
+        self.correct_answer = correct_answer
 
 question_pool = [
-    Question("What is the correct way to define a variable in Python?", ["int x = 10;", "x := 10", "x = 10", "let x = 10"], "x = 10"),
-    Question("Which of the following is a valid Python data type?", ["integer", "float", "double", "decimal"], "float"),
-    Question("How do you create a list in Python?", ["my_list = (1, 2, 3)", "my_list = [1, 2, 3]", "my_list = {1, 2, 3}", "my_list = <1, 2, 3>"], "my_list = [1, 2, 3]"),
-    Question("Which loop continues executing a block of code as long as a specified condition is true?", ["do-while loop", "for loop", "while loop", "until loop"], "while loop"),
-    Question("What is the correct way to define a function in Python?", ["function my_function():", "def my_function():", "func my_function():", "define my_function():"], "def my_function():"),
-    Question("Which of the following is a valid string in Python?", ['"Hello, World!"', "'Hello, World!'", "Hello, World!", "string('Hello, World!')"], "'Hello, World!'"),
-    Question("Which of the following is NOT a valid way to create a dictionary in Python?", ["my_dict = dict()", "my_dict = {}", "my_dict = {key: value}", "my_dict = [key: value]"], "my_dict = [key: value]"),
-    Question("How do you access the first element of a list named 'my_list'?", ["my_list[0]", "my_list(0)", "my_list(1)", "my_list[1]"], "my_list[0]"),
-    Question("What is the output of the following code snippet: 'print(5 // 2)'?", ["2", "2.5", "3", "None of the above"], "2"),
-    Question("What is the output of the following code snippet: 'print(2 * 3 ** 2)'?", ["36", "18", "12", "9"], "18"),
+    Question("What is the output of print(1 + 2)?", ["3", "1", "2"], "3"),
+    Question("Which of the following is a valid variable name?", ["1variable", "_variable", "variable!"], "_variable"),
+    Question("What is the output of the following code?\nprint(type(3.14))", ["<class 'float'>", "<class 'int'>", "<class 'str'>"], "<class 'float'>"),
+    Question("What is the output of print(5 // 2)?", ["2.5", "2", "3"], "2"),
+    Question("Which keyword is used to define a function in Python?", ["func", "def", "function"], "def"),
+    Question("Which data structure is used to store key-value pairs in Python?", ["List", "Tuple", "Dictionary"], "Dictionary"),
+    Question("How do you create a list with the values 1, 2, 3?", ["list(1, 2, 3)", "[1, 2, 3]", "{1, 2, 3}"], "[1, 2, 3]"),
+    Question("What is the output of the following code?\nprint('Hello, ' + 'World!')", ["Hello, World!", "Hello,  World!", "SyntaxError"], "Hello, World!"),
+    Question("Which of the following is NOT a valid comparison operator in Python?", ["==", "<>", "<="], "<>"),
+    Question("What is the output of the following code?\nprint('Python'.upper())", ["PYTHON", "python", "Python"], "PYTHON"),
+    Question("Which loop continues to execute as long as the condition is True?", ["for loop", "while loop", "do-while loop"], "while loop"),
+    Question("What is the output of the following code?\nprint('Hello\\nWorld')", ["Hello\nWorld", "Hello\\nWorld", "Hello World"], "Hello\\nWorld"),
+    Question("Which of the following is a Python comment?", ["// This is a comment", "# This is a comment", "/* This is a comment */"], "# This is a comment"),
+    Question("Which function is used to get input from the user?", ["input()", "get_input()", "read()"], "input()"),
+    Question("What is the output of the following code?\nprint(3 ** 2)", ["9", "6", "1"], "9"),
 ]
 
-def ask_question(question):
+def display_question(question):
     print(question.question)
-    for i, option in enumerate(question.options):
-        print(f"{i+1}. {option}")
-    answer = int(input("Select the correct option (1-4): ")) - 1
-    return question.options[answer] == question.answer
+    for idx, choice in enumerate(question.choices):
+        print(f"{idx + 1}. {choice}")
 
-def main():
+def get_user_input():
+    return input("Enter your answer: ")
+
+def evaluate_response(user_response, correct_answer):
+    return user_response == correct_answer
+
+def play_game():
     score = 0
-    random.shuffle(question_pool)
-
-    for question in question_pool:
-        if ask_question(question):
-            print("Correct!")
+    while True:
+        question = random.choice(question_pool)
+        display_question(question)
+        user_response = get_user_input()
+        if evaluate_response(user_response, question.correct_answer):
             score += 1
+            print("Correct!")
         else:
-            print("Incorrect. Better luck next time!")
-        print()
-
-    print(f"Your final score: {score}/{len(question_pool)}")
+            print(f"Sorry, the correct answer is {question.correct_answer}.")
+        
+        print(f"Your score: {score}")
+        play_again = input("Do you want to play again? (Y/N) ")
+        if play_again.lower() != 'y':
+            break
 
 if __name__ == "__main__":
-    main()
+    play_game()
